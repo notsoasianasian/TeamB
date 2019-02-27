4b564cbf21ca7c4b709924bfc62cf51e9a540a6d
CLUnity <44986121+CLUnity@users.noreply.github.com>
2019-02-27 14:26:59 -0500

    Adding Base UI's
    Adding the Base of the UI's will edit them when I have more time.

---
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button StartGameButton;
+    public Button LoadGameButton;
+    public Button OptionsButton;
+    public Button ExitButton;
+
+    public void StartGame()
+    {
+       //SceneManager.LoadScene(1);
+    }
+    public void LoadGame()
+    {
+       
+    }
+    public void Options()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

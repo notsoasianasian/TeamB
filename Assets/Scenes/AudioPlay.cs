710a204f2c85fd2f713d5fb88f710a243cedbad2
notsoasianasian <44407935+notsoasianasian@users.noreply.github.com>
2019-04-18 09:25:31 -0400

    Add files via upload

---
+++ Assets/Scenes/AudioPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+
+{ //public AudioClip sound;
+    public AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+  
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "gem")
+        {
+
+            audio.Play();
+        }
+    }
+}
---
+++ Assets/Scenes/gemPart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gemPart : MonoBehaviour
+{
+    public GameObject dustCloud;
+   // public AudioSource audio;
+
+    void Start()
+    {
+       // audio = GetComponent<AudioSource>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+           // audio.Play();
+            Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+
+        }
+
+    }
+}
